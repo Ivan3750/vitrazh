6369cb44b8a1304c13ccacedeebf0dc774ea3234
Ivan Kohan <kohan3750@gmail.com>
2025-07-11 19:09:43 +0200

    Update DynamicCalculator.jsx

--- app/components/DynamicCalculator.jsx
+++ app/components/DynamicCalculator.jsx
@@ -13,7 +13,7 @@ export default function DynamicCalculator({ productType }) {
     hasBrake: false,
   });
 
-  const isFrame = productType === "ramni-sitky";
+  const isFrame = productType === "ramna-sitka";
   const isDoorMesh = productType === "dverni-sitky";
   const isAlyumin = productType === "alyuminiievi-sitky";
   const isPlisse = productType === "plisse-sitky";
@@ -42,6 +42,7 @@ export default function DynamicCalculator({ productType }) {
     ));
 
   return (
+    <section className="p-4">
     <div className="bg-[#f8f7f0] px-6 py-10 rounded-md max-w-2xl mx-auto mt-10">
       <h3 className="text-2xl mb-6 text-center">Розрахувати замовлення</h3>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
@@ -132,6 +133,21 @@ export default function DynamicCalculator({ productType }) {
                 { value: "brown", label: "Коричневий" },
                 { value: "anthracite", label: "Антрацит" },
               ])}
+            </select>
+          </div>
+        )}
+        {(isFrame) && (
+          <div className="flex flex-col">
+            <label htmlFor="type">Тип:</label>
+            <select
+              name="type"
+              value={form.type}
+              onChange={handleChange}
+              className="p-2 border rounded"
+            >
+                <option value="11x32">11x32</option>
+              <option value="lux">lux</option>
+  
             </select>
           </div>
         )}
@@ -230,5 +246,6 @@ export default function DynamicCalculator({ productType }) {
         </div>
       </form>
     </div>
+    </section>
   );
 }
